f836c09b90efff1f5e4693d7aebf18c18493d255
john doe <durosier5@hotmail.com>
2016-09-29 23:41:43 -0400

    first commit

---
+++ src/testing/Driver.java
@@ -0,0 +1,14 @@
+package testing;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		LCD.drawString("Plugin Test", 0, 4);
+		Delay.msDelay(5000);
+
+	}
+
+}
